b95c67c5faa35ebe165e79e470ab1898c01a9a2f
E9C50 <jasonfu516@outlook.com>
2024-12-11 03:24:41 +0800

    fix bug

--- utils.room.js
+++ utils.room.js
@@ -44,65 +44,66 @@ var roomUtils = {
         var targetResource = sources[resourceIndex];
         var targetPos = targetResource.pos;
 
+        // const surroundingPositions = [
+        //     new RoomPosition(targetPos.x, targetPos.y - 1, targetPos.roomName),      // 上
+        //     new RoomPosition(targetPos.x, targetPos.y + 1, targetPos.roomName),      // 下
+        //     new RoomPosition(targetPos.x - 1, targetPos.y, targetPos.roomName),      // 左
+        //     new RoomPosition(targetPos.x + 1, targetPos.y, targetPos.roomName),      // 右
+        // ];
+
+        // const terrains = surroundingPositions
+        //     .map(pos => room.lookForAt(LOOK_TERRAIN, pos))
+        //     .filter(terrain => terrain != 'wall');
+
+        // var positionCount = terrains.length
+
+        // var topLeft = [
+        //     new RoomPosition(targetPos.x - 1, targetPos.y - 1, targetPos.roomName),  // 左上
+        //     new RoomPosition(targetPos.x, targetPos.y - 1, targetPos.roomName),      // 上
+        //     new RoomPosition(targetPos.x - 1, targetPos.y, targetPos.roomName),      // 左
+        // ].map(pos => room.lookForAt(LOOK_TERRAIN, pos)).filter(terrain => terrain != 'wall').length
+
+        // var topRight = [
+        //     new RoomPosition(targetPos.x + 1, targetPos.y - 1, targetPos.roomName),  // 右上
+        //     new RoomPosition(targetPos.x, targetPos.y - 1, targetPos.roomName),      // 上
+        //     new RoomPosition(targetPos.x + 1, targetPos.y, targetPos.roomName),      // 右
+        // ].map(pos => room.lookForAt(LOOK_TERRAIN, pos)).filter(terrain => terrain != 'wall').length
+
+
+        // var bottomLeft = [
+        //     new RoomPosition(targetPos.x - 1, targetPos.y + 1, targetPos.roomName),  // 左下
+        //     new RoomPosition(targetPos.x, targetPos.y + 1, targetPos.roomName),      // 下
+        //     new RoomPosition(targetPos.x - 1, targetPos.y, targetPos.roomName),      // 左
+        // ].map(pos => room.lookForAt(LOOK_TERRAIN, pos)).filter(terrain => terrain != 'wall').length
+
+        // var bottomRight = [
+        //     new RoomPosition(targetPos.x + 1, targetPos.y + 1, targetPos.roomName),  // 右下
+        //     new RoomPosition(targetPos.x, targetPos.y + 1, targetPos.roomName),      // 下
+        //     new RoomPosition(targetPos.x + 1, targetPos.y, targetPos.roomName),      // 右
+        // ].map(pos => room.lookForAt(LOOK_TERRAIN, pos)).filter(terrain => terrain != 'wall').length
+
+        // if (topLeft == 3) { positionCount += 1 }
+        // if (topRight == 3) { positionCount += 1 }
+        // if (bottomLeft == 3) { positionCount += 1 }
+        // if (bottomRight == 3) { positionCount += 1 }
+
+        // return positionCount
+
         const surroundingPositions = [
+            new RoomPosition(targetPos.x - 1, targetPos.y - 1, targetPos.roomName),  // 左上
             new RoomPosition(targetPos.x, targetPos.y - 1, targetPos.roomName),      // 上
-            new RoomPosition(targetPos.x, targetPos.y + 1, targetPos.roomName),      // 下
+            new RoomPosition(targetPos.x + 1, targetPos.y - 1, targetPos.roomName),  // 右上
             new RoomPosition(targetPos.x - 1, targetPos.y, targetPos.roomName),      // 左
             new RoomPosition(targetPos.x + 1, targetPos.y, targetPos.roomName),      // 右
+            new RoomPosition(targetPos.x - 1, targetPos.y + 1, targetPos.roomName),  // 左下
+            new RoomPosition(targetPos.x, targetPos.y + 1, targetPos.roomName),      // 下
+            new RoomPosition(targetPos.x + 1, targetPos.y + 1, targetPos.roomName),  // 右下
         ];
 
         const terrains = surroundingPositions
             .map(pos => room.lookForAt(LOOK_TERRAIN, pos))
             .filter(terrain => terrain != 'wall');
-
-        var positionCount = terrains.length
-
-        var topLeft = [
-            new RoomPosition(targetPos.x - 1, targetPos.y - 1, targetPos.roomName),  // 左上
-            new RoomPosition(targetPos.x, targetPos.y - 1, targetPos.roomName),      // 上
-            new RoomPosition(targetPos.x - 1, targetPos.y, targetPos.roomName),      // 左
-        ].map(pos => room.lookForAt(LOOK_TERRAIN, pos)).filter(terrain => terrain != 'wall').length
-
-        var topRight = [
-            new RoomPosition(targetPos.x + 1, targetPos.y - 1, targetPos.roomName),  // 右上
-            new RoomPosition(targetPos.x, targetPos.y - 1, targetPos.roomName),      // 上
-            new RoomPosition(targetPos.x + 1, targetPos.y, targetPos.roomName),      // 右
-        ].map(pos => room.lookForAt(LOOK_TERRAIN, pos)).filter(terrain => terrain != 'wall').length
-
-
-        var bottomLeft = [
-            new RoomPosition(targetPos.x - 1, targetPos.y + 1, targetPos.roomName),  // 左下
-            new RoomPosition(targetPos.x, targetPos.y + 1, targetPos.roomName),      // 下
-            new RoomPosition(targetPos.x - 1, targetPos.y, targetPos.roomName),      // 左
-        ].map(pos => room.lookForAt(LOOK_TERRAIN, pos)).filter(terrain => terrain != 'wall').length
-
-        var bottomRight = [
-            new RoomPosition(targetPos.x + 1, targetPos.y + 1, targetPos.roomName),  // 右下
-            new RoomPosition(targetPos.x, targetPos.y + 1, targetPos.roomName),      // 下
-            new RoomPosition(targetPos.x + 1, targetPos.y, targetPos.roomName),      // 右
-        ].map(pos => room.lookForAt(LOOK_TERRAIN, pos)).filter(terrain => terrain != 'wall').length
-
-        console.log(positionCount);
-
-        if (topLeft == 3) { positionCount += 1 }
-        if (topRight == 3) { positionCount += 1 }
-        if (bottomLeft == 3) { positionCount += 1 }
-        if (bottomRight == 3) { positionCount += 1 }
-        console.log(positionCount);
-        console.log('=================');
-
-        // const surroundingPositions = [
-        //     new RoomPosition(targetPos.x - 1, targetPos.y - 1, targetPos.roomName),  // 左上
-        //     new RoomPosition(targetPos.x, targetPos.y - 1, targetPos.roomName),      // 上
-        //     new RoomPosition(targetPos.x + 1, targetPos.y - 1, targetPos.roomName),  // 右上
-        //     new RoomPosition(targetPos.x - 1, targetPos.y, targetPos.roomName),      // 左
-        //     new RoomPosition(targetPos.x + 1, targetPos.y, targetPos.roomName),      // 右
-        //     new RoomPosition(targetPos.x - 1, targetPos.y + 1, targetPos.roomName),  // 左下
-        //     new RoomPosition(targetPos.x, targetPos.y + 1, targetPos.roomName),      // 下
-        //     new RoomPosition(targetPos.x + 1, targetPos.y + 1, targetPos.roomName),  // 右下
-        // ];
-
-        return positionCount
+        return 8 - terrains.length
     }
 }
 module.exports = roomUtils;
